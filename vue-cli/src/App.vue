2b8d0ac8e58e5cbe21db450731030f5ea0734f9d
prpr48 <prpr48@naver.com>
2023-02-17 15:47:19 +0900

    app-header component

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,11 +1,17 @@
 <template>
+  <app-header></app-header>
   <div>{{ str }}</div>
 </template>
 
 <script>
+import AppHeader from "./components/AppHeader.vue";
+
 export default {
   data: function () {
     return { str: "hi" };
+  },
+  components: {
+    "app-header": AppHeader,
   },
 };
 </script>
